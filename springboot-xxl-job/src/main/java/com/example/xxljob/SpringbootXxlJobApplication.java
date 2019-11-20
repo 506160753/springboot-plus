3f949e3ec6ac3f6aefe84974148387e79772c6ae
Liuxing <a506160753a@gmail.com>
2019-11-20 23:51:59 +0800

    集成xxl-job任务调度

--- springboot-xxl-job/src/main/java/com/example/xxljob/SpringbootXxlJobApplication.java
+++ springboot-xxl-job/src/main/java/com/example/xxljob/SpringbootXxlJobApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootXxlJobApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(SpringbootXxlJobApplication.class, args);
     }
 
