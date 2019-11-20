83ee281ef9f6953da6fee3744e40995cba717625
Liuxing <a506160753a@gmail.com>
2019-11-20 23:49:53 +0800

    集成Mybatis

--- springboot-mybatis/src/main/java/com/example/mybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/example/mybatis/SpringbootMybatisApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMybatisApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(SpringbootMybatisApplication.class, args);
     }
 
