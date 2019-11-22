fa8921295dba2008882e6e59e35bfa713c5b012d
Liuxing <a506160753a@gmail.com>
2019-11-22 15:23:35 +0800

    commit

---
+++ 12.SpringBoot-Admin/SpringBoot-Admin-Server/src/main/java/com/example/demo/SpringBootAdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminServerApplication {
+
+   public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+}
