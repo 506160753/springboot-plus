0a96775f99808172d554fb3909d27f8d427d3755
Liuxing <a506160753a@gmail.com>
2019-11-25 16:47:54 +0800

    commit

---
+++ 19.SpringBoot-Exception/src/main/java/com/example/demo/controller/ExceptionController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.BusinessException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @GetMapping("/RuntimeException")
+    public void RuntimeException() {
+        throw new RuntimeException("有个业务异常");
+    }
+
+    @GetMapping("/BusinessException")
+    public void BusinessException () {
+        throw new BusinessException("有个自定义异常");
+    }
+}
